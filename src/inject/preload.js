e74adc7e932e8debd965e0027a43c986bb38d78e
oblank <dyh1919@gmail.com>
2016-03-31 15:55:23 +0800

    优化代码组织方式

--- src/inject/preload.js
+++ src/inject/preload.js
@@ -2,6 +2,7 @@
 const ipcRenderer = require('electron').ipcRenderer;
 const webFrame = require('web-frame');
 const MenuHandler = require('../handler/menu');
+const genShareMenu = require('./share_menu');
 
 const lock = (object, key, value) => Object.defineProperty(object, key, {
   get: () => value,
@@ -78,9 +79,8 @@ injectBundle.getBadgeJS = () => {
 };
 
 injectBundle.appendMenu = () => {
-  var curr_pos, title = "";
-  var hash_tag = "微信好文分享";
-  var menu, reader;
+  let menu, reader;
+  let curr_pos, title, menu_html;
   setInterval(() => {
     reader = document.getElementById("reader");
     menu = reader ? document.getElementById("mmpop_reader_menu") : false;
@@ -93,50 +93,8 @@ injectBundle.appendMenu = () => {
       }
       if ((curr_pos != reader.src) || ($(".reader_menu .dropdown_menu > li").length < 5)) {
         curr_pos = reader.src;
-        var share_url = encodeURIComponent(reader.src);
-        var share_title = encodeURIComponent(title);
-        var title4weibo = encodeURIComponent(`${title} #${hash_tag}#`);
-        var title4twitter = encodeURIComponent(`${title} #${hash_tag}`);
-        var title4fb = title4twitter;
-        //var html = reader.contentDocument.body.innerHTML;
-        var share_weibo = `http://service.weibo.com/share/share.php?url=${share_url}&title=${title4weibo}#&searchPic=yes`;
-        var share_twitter = `https://twitter.com/intent/tweet?text=${title4twitter}&url=${share_url}&original_referer=`;
-        var share_email = `mailto:?&subject=${share_title}&body=${share_title}%0A${share_url}`;
-        var share_qzone = `http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=${share_url}&title=${share_title}&pics=&summary=`;
-        var share_facebook = `https://www.facebook.com/sharer/sharer.php?s=100&p%5Btitle%5D=${title4fb}&p%5Bsummary%5D=%21&p%5Burl%5D=${share_url}&p%5Bimages%5D=`;
-        var menuHtml = `
-          <li>
-            <a href="javascript:;" target="_blank" onclick="javascript:window.open('${share_weibo}', '_blank'); return;">
-              <i class="menuicon_copylink"></i>
-              分享到微博
-            </a>
-          </li>
-          <li>
-            <a href="javascript:;" target="_blank" onclick="javascript:window.open('${share_twitter}', '_blank'); return;">
-              <i class="menuicon_copylink"></i>
-              分享到Twitter
-            </a>
-          </li>
-          <li>
-            <a href="javascript:;" target="_blank" onclick="javascript:window.open('${share_qzone}', '_blank'); return;">
-              <i class="menuicon_copylink"></i>
-              分享到QQ空间
-            </a>
-          </li>
-          <li>
-            <a href="javascript:;" target="_blank" onclick="javascript:window.open('${share_facebook}', '_blank'); return;">
-              <i class="menuicon_copylink"></i>
-              分享到FaceBook
-            </a>
-          </li>
-          <li>
-            <a href="javascript:;" target="_blank" onclick="javascript:window.open('${share_email}', '_blank'); return;">
-              <i class="menuicon_copylink"></i>
-              邮件分享
-            </a>
-          </li>
-        `;
-        $(".reader_menu .dropdown_menu").prepend(menuHtml);
+        menu_html = genShareMenu({url: reader.src, title:title});
+        $(".reader_menu .dropdown_menu").prepend(menu_html);
       }
     }
 
